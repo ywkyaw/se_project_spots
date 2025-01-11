25d63313a7d94daa415b0f9338503f20b4e229cf
YE KYAW <YEWKYAW@GMAIL.COM>
2025-01-10 19:45:07 -0600

    Proj4_Stg3

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,22 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit");
+
+const editProfileModal = document.querySelector("#edit-profile-modal");
+
+const profileCloseButton = document.querySelector(".modal__close-btn");
+
+const closeProfileModal = document.querySelector("#edit-profile-modal");
+
+function openModal() {
+  editProfileModal.classList.add("modal__opened");
+}
+
+function closeModal() {
+  closeProfileModal.classList.remove("modal__opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+profileCloseButton.addEventListener("click", closeModal);
